09b3da7a28172949c7b356513cc207e466dda66b
Jess Arschoot <>
2017-12-03 10:01:06 +0100

    change in article html

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,6 +1,8 @@
 import { Article } from '../models/Article';
 import { ArticleService } from '../services/article.service';
 import { Component, Input, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { User } from '../models/User';
 declare var jquery: any;
 declare var $: any;
 
@@ -11,11 +13,24 @@ declare var $: any;
 })
 export class ArticleComponent implements OnInit {
   @Input() public article: Article
-  constructor(private service: ArticleService) {
+  _user: User;
+  _like: Boolean;
+  constructor(private service: ArticleService, private userService: UserService) {
    }
 
   ngOnInit() {
-    
+    this.userService.getUser(JSON.parse(localStorage.getItem('currentUser')).username).subscribe(data => {
+      this._user = new User(data.name, data.username, data.picture)
+    });
+    this.article.likes.forEach(item =>{
+      console.log(item);
+      if(item == this._user.username){
+        this._like = true;
+      }
+      else{
+        this._like = false;
+      }
+    });
   }
 
 }
