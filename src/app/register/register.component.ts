3abba4daf13b6b66e3a0c663185cc47864e7deb9
Jess Arschoot <>
2017-12-06 14:10:47 +0100

    styling register page

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -27,7 +27,6 @@ export class RegisterComponent implements OnInit {
   
   @ViewChild('fileInput') fileInput;
   public user: FormGroup;
-  public _picture: Boolean = false;
   
   get passwordControl(): FormControl {
     return <FormControl>this.user.get('passwordGroup').get('password');
@@ -58,7 +57,6 @@ export class RegisterComponent implements OnInit {
   }
 
    previewFile() {
-    this._picture = true;
     var preview = document.getElementById('previewPic');
     var file    = this.fileInput.nativeElement.files[0];
     var reader  = new FileReader();
